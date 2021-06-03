52ccdb137af6a4181e7ccb3d747e098da3c897f7
YYUUK <aaai0912@naver.com>
2021-06-03 23:07:09 +0900

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import requests
+
+url = "http://apis.data.go.kr/1360000/AsosDalyInfoService/getWthrDataList?serviceKey=jCAu%2By2rHxF3JN9k%2BXwhS%2FRDwWIRS9O3ln2p%2Fdxx%2BRNj3deacKcMXUQv9Cn5EJSJet3sBrb41dp%2BJLK%2FNpfk%2Bw%3D%3D&numOfRows=10&pageNo=1&dataType=JSON&dataCd=ASOS&dateCd=DAY&startDt=20100101&endDt=20100102&stnIds=108"
+
+response = requests.get(url)
+responseJson = response.json()
+
+
+print()
