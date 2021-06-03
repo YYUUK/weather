492dbe572c1e441d087f1d2c0e5ddaaa40bc7063
YYUUK <aaai0912@naver.com>
2021-06-04 03:31:26 +0900

    csv 작업 전까지

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import requests
+import pprint
+import pandas as pd
 
 url = "http://apis.data.go.kr/1360000/AsosDalyInfoService/getWthrDataList?serviceKey=jCAu%2By2rHxF3JN9k%2BXwhS%2FRDwWIRS9O3ln2p%2Fdxx%2BRNj3deacKcMXUQv9Cn5EJSJet3sBrb41dp%2BJLK%2FNpfk%2Bw%3D%3D&numOfRows=10&pageNo=1&dataType=JSON&dataCd=ASOS&dateCd=DAY&startDt=20100101&endDt=20100102&stnIds=108"
 
@@ -6,4 +8,6 @@ response = requests.get(url)
 responseJson = response.json()
 
 
-print()
+df = pd.json_normalize(responseJson['response'])
+pprint.pprint(responseJson)
+print(df)
