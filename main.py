3f48ac201831df336f4fd59f6dbab39c647bd856
YYUUK <aaai0912@naver.com>
2021-06-06 01:57:16 +0900

    완료

--- main.py
+++ main.py
@@ -15,7 +15,11 @@ avgTa = 평균 기온  avgWs = 평균 풍속  avgRhm = 평균 상대습도  stnN
 result = ['avgTa', 'avgWs', 'avgRhm']
 average = [0, 0, 0]
 
+f = open('test_file.csv', 'w', newline='')
+wr = csv.writer(f)
+
 for i in region:
+    sample_list = []
     avg = 0
     url = "http://apis.data.go.kr/1360000/AsosDalyInfoService/getWthrDataList?serviceKey=jCAu%2By2rHxF3JN9k%2BXwhS%2FRDwWIRS9O3ln2p%2Fdxx%2BRNj3deacKcMXUQv9Cn5EJSJet3sBrb41dp%2BJLK%2FNpfk%2Bw%3D%3D&numOfRows=10&pageNo=1&dataType=JSON&dataCd=ASOS&dateCd=DAY&startDt=20200610&endDt=20200620&stnIds="+str(i)
     response = requests.get(url)
@@ -26,5 +30,10 @@ for i in region:
         average[2] = average[2] + float(responseJson['response']['body']['items']['item'][j]['avgRhm'])
     for k in range(3):
         average[k] = average[k] / 10
-    print(responseJson['response']['body']['items']['item'][0]['stnNm'])
-    print(average)
+    sample_list.append(responseJson['response']['body']['items']['item'][0]['stnNm'])
+    sample_list.append(str(round(average[0], 2)))
+    sample_list.append(str(round(average[1], 2)))
+    sample_list.append(str(round(average[2], 2)))
+    wr.writerows(sample_list)
+
+f.close()
